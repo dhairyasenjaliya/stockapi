4087d4f5f697b446681ecc1f2b3fc7302b348c75
Dhairya <47522946+dhairyasenjaliya@users.noreply.github.com>
2019-06-04 14:02:01 +0530

    sorting

--- app/Http/Controllers/stockmanager.php
+++ app/Http/Controllers/stockmanager.php
@@ -64,7 +64,7 @@ class stockmanager extends Controller
      
     public function getsector()
     {
-        $data = Sector::all('id','name')->orderBy('name')->get();
+        $data = Sector::all('id','name')->sortBy('name')->get();
         return response()->json($data);
     }
 
